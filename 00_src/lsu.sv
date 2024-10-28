795588f6464e53e51c0355c0499af42e1037c6a8
jesuisfim <127768648+jesuisfim@users.noreply.github.com>
2024-10-28 23:50:00 +0700

    Fix lsu.sv
    Fix: Ability to display 0xA, B, C, D, E, F using 7-segment
    
    Pending items:
    - SDRAM controller (waiting for aHai's guiding docs)
    - LCD controller

--- 00_src/lsu.sv
+++ 00_src/lsu.sv
@@ -178,39 +178,25 @@ module lsu
    //---------------------------
    function [6:0] bcd_to_7seg;
       input [3:0] bcd;
-      // when bcd = 0, seg = 7'b1000000	
-      // when bcd = 1, seg = 7'b1111001	
-      // when bcd = 2, seg = 7'b0100100	
-      // when bcd = 3, seg = 7'b0110000	
-      // when bcd = 4, seg = 7'b0011001	
-      // when bcd = 5, seg = 7'b0010010	
-      // when bcd = 6, seg = 7'b0000010	
-      // when bcd = 7, seg = 7'b1111000	
-      // when bcd = 8, seg = 7'b0000000	
-      // when bcd = 9, seg = 7'b0010000	
 
-      bcd_to_7seg[0] = ((~bcd[3])&(~bcd[2])&(~bcd[1])&bcd[0]) | 
-                       ((~bcd[3])&bcd[2]&(~bcd[1])&(~bcd[0]));
-
-      bcd_to_7seg[1] = ((~bcd[3])&bcd[2]&(~bcd[1])&bcd[0]) | 
-                       ((~bcd[3])&bcd[2]&bcd[1]&(~bcd[0]));    
-
-      bcd_to_7seg[2] = ((~bcd[3])&(~bcd[2])&bcd[1]&(~bcd[0]));
-      
-      bcd_to_7seg[3] = ((~bcd[3])&(~bcd[2])&(~bcd[1])&bcd[0]) | 
-                       ((~bcd[3])&bcd[2]&(~bcd[1])&(~bcd[0])) | 
-                       ((~bcd[3])&bcd[2]&bcd[1]&bcd[0]);
-      
-      bcd_to_7seg[4] = ((~bcd[3])&bcd[0])           | 
-                       ((~bcd[3])&bcd[2]&(~bcd[1])) | 
-                       (~(bcd[2])&(~bcd[1])&bcd[0]);
-      
-      bcd_to_7seg[5] = ((~bcd[3])&(~bcd[2])&bcd[0]) | 
-                       ((~bcd[3])&(~bcd[2])&bcd[1]) | 
-                       ((~bcd[3])&bcd[1]&bcd[0]);
-
-      bcd_to_7seg[6] = ((~bcd[3])&(~bcd[2])&(~bcd[1])) |
-                       ((~bcd[3])&bcd[2]&bcd[1]&bcd[0]);
+      case(bcd)
+         4'h0: bcd_to_7seg = 7'b1000000;
+         4'h1: bcd_to_7seg = 7'b1111001;	
+         4'h2: bcd_to_7seg = 7'b0100100; 	
+         4'h3: bcd_to_7seg = 7'b0110000; 	
+         4'h4: bcd_to_7seg = 7'b0011001; 	
+         4'h5: bcd_to_7seg = 7'b0010010; 	
+         4'h6: bcd_to_7seg = 7'b0000010; 	
+         4'h7: bcd_to_7seg = 7'b1111000; 	
+         4'h8: bcd_to_7seg = 7'b0000000; 	
+         4'h9: bcd_to_7seg = 7'b0011000; 	
+         4'ha: bcd_to_7seg = 7'b0001000;
+         4'hb: bcd_to_7seg = 7'b0000011;
+         4'hc: bcd_to_7seg = 7'b1000110;
+         4'hd: bcd_to_7seg = 7'b0100001;
+         4'he: bcd_to_7seg = 7'b0000110;
+         4'hf: bcd_to_7seg = 7'b0001110;
+      endcase      
    endfunction // End 7-SEG display control
 
 
